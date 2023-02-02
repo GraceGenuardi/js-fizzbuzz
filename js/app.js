b2b1fa3c7de05386eb45531234e70e19f32e9e55
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-02 13:48:58 +0100

    EDIT JS -da 1 a 100

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log('IN LOOP!')
+
+
+let number
+
+
+
+//1- Scrivi e stampa in colsole i numeri da 1 a 100
+
+ for (let i = 0; i <= 100; i++) {
+ 	console.log(i)
+ }
+
+console.log('fine del ciclo, sei arrivato a 100. Bravo!')
+
